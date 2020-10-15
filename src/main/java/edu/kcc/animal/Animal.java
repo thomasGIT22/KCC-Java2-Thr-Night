4221bf9948354e61267813fd8395e6b0c77bc46e
Cash Carlson <cash12121@gmail.com>
2020-10-15 18:29:14 -0500

    Added dateValidator

--- src/main/java/edu/kcc/animal/Animal.java
+++ src/main/java/edu/kcc/animal/Animal.java
@@ -29,7 +29,6 @@ public class Animal{
     private LocalDate dateAdded;
     private LocalDateTime lastFeedingTime;
 
-    // TODO: Need full constructor (Cash)
     public Animal(String id, String name, String species, 
             String gender, int age, boolean fixed, int legs, BigDecimal weight,
             LocalDate dateAdded, LocalDateTime lastFeedingTime) {
@@ -137,8 +136,12 @@ public class Animal{
         this.dateAdded = dateAdded;
     }
     
-    // TODO: Need dateValidator method (Cash)
-        // only allow dates up to a week in the past
+    public void dateValidator(LocalDate date) {
+        //Check if date is more than a week in the past
+        if(date.isBefore(LocalDate.now().minusWeeks(1))){
+            throw new IllegalArgumentException();
+        }
+    }
     
     
     // TODO: Need getLastFeedingTime method (Sheryl)
