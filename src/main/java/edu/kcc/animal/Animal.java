2b1a9fd80e9a8838b74430b45ae601ffa7e7a05a
thomasGIT22 <patronus.me@gmail.com>
2020-10-22 19:05:46 -0500

    trying to get this to work

---
+++ src/main/java/edu/kcc/animal/Animal.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kcc.animal;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author k0519415
+ */
+public class Animal implements Comparable<Animal>{
+
+    private String id;
+    private static List<String> idList = new ArrayList<>();
+    private String name;
+    private String species;
+    private String gender;
+    private int age;
+    private boolean fixed;
+    private int legs;
+    private BigDecimal weight;
+    private LocalDate dateAdded;
+    private LocalDateTime lastFeedingTime;
+
+    public Animal(String id, String name, String species, 
+            String gender, int age, boolean fixed, int legs, BigDecimal weight,
+            LocalDate dateAdded, LocalDateTime lastFeedingTime) {
+        setId(id);
+        idList.add(id);
+        setName(name);
+        setSpecies(species);
+        setGender(gender);
+        setAge(age);
+        setFixed(fixed);
+        setLegs(legs);
+        setWeight(weight);
+        setDateAdded(dateAdded);
+        setLastFeedingTime(lastFeedingTime);
+    }
+    
+    public Animal() {
+        id = "0";
+        idList.add(id);
+        name = "Unknown";
+        species = "Unknown";
+        gender = "Unknown";
+        age = 0;
+        fixed = false;
+        legs = 4;
+        weight = BigDecimal.valueOf(0);
+        dateAdded = LocalDate.of(2020, 9, 1); // September 1, 2020
+        lastFeedingTime = LocalDateTime.of(2020, 10, 1, 23, 59); // October 1, 2020 at 11:59pm
+    }
+    
+    // TODO: Need getId method
+    public String getId() {
+        return "";
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setId(String id) {
+        this.id = id;
+    }
+        
+    // TODO: Fix idValidator - Can't compare strings with ==. Move second if statement out of the loop. Set to private.
+    public void idValidator(String id){
+        
+        for (int i = 0; i < idList.size(); i++) {
+            if (id == idList.get(i)){
+                throw new IllegalArgumentException("The id enterd is not a valid id. "
+                        + "make sure the id is not allready in the id list.");
+            }
+            if(id != "0"){
+                throw new IllegalArgumentException("The id can only "
+                        + "change if it's \"0\"." );
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+  
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public  String getGender(){
+        return gender;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setGender(String g){
+        gender = g;
+    }
+    
+    // TODO: Need genderValidator method - Only allow male and female. Only allow it to change if it's "Unknown".
+    private void genderValidator() {
+        
+    }
+    
+    public String getSpecies() {
+        return species;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setSpecies(String species){
+        this.species = species;
+    }
+    
+    // TODO: Need speciesValidator method - Only allow cat and dog. Only allow it to change if it's "Unknown".
+    private void speciesValidator() {
+        
+    }
+    
+    public int getAge(){
+        return age;
+    }
+    
+    // TODO: Need setAge method. Call the validator method before assigning the value
+    public void setAge(int age) {
+        
+    }
+    
+    private void validateAge(int age){
+        if(age < 0 || age > 100){
+            throw new IllegalArgumentException("Age must be between 0 and 100");
+        }
+    }
+    
+    // TODO: Need getFixed method
+    public boolean getFixed() {
+        return false;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setFixed (boolean fix){
+        fixed = fix;
+    }
+    
+    // TODO: Need fixedValidator method - don't allow an animal that is already fixed to be fixed again 
+    private void fixedValidator() {
+        
+    }
+    
+    public int getLegs() {
+        return legs;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setLegs(int legs){
+        this.legs = legs;
+    }
+    
+    // TODO: Need legsValidator method - only allow legs 0 to 4
+    private void legsValidator() {
+        
+    }
+    
+    public BigDecimal getWeight() {
+        return weight;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setWeight(BigDecimal animalWeight){
+        weight = animalWeight;
+    }
+    
+    // TODO: Need weightValidator method - only allow weight 0.0 to 1000.0 
+    private void weightValidator() {
+        
+    }
+
+    public LocalDate getDateAdded(){
+        return dateAdded;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+    
+    // TODO - Include a string inside the parenthesis of the IllegalArgumentException to say what is wrong. Set to private.
+    // TODO - Don't allow future dates.
+    public void dateValidator(LocalDate date) {
+        if(date.isBefore(LocalDate.now().minusWeeks(1))){
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    public LocalDateTime getLastFeedingTime() {
+        return lastFeedingTime;
+    }
+    
+    // TODO: Call the validator method before assigning the value
+    public void setLastFeedingTime(LocalDateTime ldt) {
+        lastFeedingTime = ldt;
+    }
+    
+    private void feedingValidator(LocalDateTime lastFeedingTime){
+        if (LocalDateTime.now().minusDays(2).isBefore(lastFeedingTime)) {
+            throw new IllegalArgumentException("Last feeding times"
+                    + "no less than two days in the past");
+        }
+    }    
+    
+    @Override
+    public String toString(){
+        return name + " the " + gender + " " + species + " is " + age
+                + " and has a weight of " + weight.toString();
+    }
+
+    // TODO: Need compareTo method. Compare by their species first, then by their name 
+    public int compareTo(Animal other) {
+        return 0;
+    }
+    
+}
---
+++ src/test/java/edu/kcc/animal/AnimalTest.java
@@ -0,0 +1,440 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kcc.animal;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author marchauschildt
+ */
+public class AnimalTest {
+    
+    private Animal instance;
+
+    @Before
+    public void setUp() {
+        instance = new Animal();
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testGetId() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetId() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO - This should not allow a second animal to have the same id as the first animal
+    @org.junit.Test
+    public void testSetIdBad() {
+        fail("The test case is a prototype.");
+    }
+
+    @org.junit.Test
+    public void testGetName() {
+        setUp();
+        String expected = "Unknown";
+        String actual = instance.getName();
+        assertEquals(expected, actual);
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetName() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetGender() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetGender() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    public void testSetGenderNotMaleFemaleBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    public void testSetGenderMaleToFemaleBad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetSpecies() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetSpecies() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetSpeciesNotCatorDogBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetSpeciesCatToDogBad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetAge() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetAge0Good() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetAge100Good() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetAgeNegativeBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetAgeAbove100Bad() {
+        fail("The test case is a prototype.");
+    }
+
+    @org.junit.Test
+    public void testGetFixed() {
+        setUp();
+        boolean expResult = false;
+        boolean result = instance.getFixed();
+        assertEquals(expResult, result);
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetFixedFalseToTrueGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetFixedTruetoFalseBad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetLegs() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetLegsTo4Good() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLegsTo0Good() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLegsTo5Bad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLegsToNegativeBad() {
+        fail("The test case is a prototype.");
+    }
+
+    @org.junit.Test
+    public void testGetWeight() {
+        setUp();
+        BigDecimal expected = new BigDecimal(0);
+        BigDecimal actual = instance.getWeight();
+        assertEquals(expected, actual);
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetWeightToZeroGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetWeightTo1000Good() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetWeightNegativeBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetWeightAbove1000Bad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetDateAdded() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetDateAddedTodayGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetDateAddedAWeekAgoGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetDateAdded8DaysAgoBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetDateAddedTomorrowBad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testGetLastFeedingTime() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testSetLastFeedingTimeAnHourAgoGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLastFeedingTime2DaysAgoGood() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLastFeedingTime3DaysAgoBad() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testSetLastFeedingTime1HourInTheFutureBad() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testToString() {
+        fail("The test case is a prototype.");
+    }
+
+    // TODO
+    @org.junit.Test
+    public void testCompareToCattoDog() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testCompareToDogtoCat() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testCompareToCatAlphatoCatBeta() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testCompareToCatBetatoCatAlpha() {
+        fail("The test case is a prototype.");
+    }
+    
+    // TODO
+    @org.junit.Test
+    public void testCompareToCatAlphatoCatAlpha() {
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of idValidator method, of class Animal.
+     */
+    @Test
+    public void testIdValidator() {
+        System.out.println("idValidator");
+        String id = "";
+        Animal instance = new Animal();
+        instance.idValidator(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setAge method, of class Animal.
+     */
+    @Test
+    public void testSetAge() {
+        System.out.println("setAge");
+        int age = 0;
+        Animal instance = new Animal();
+        instance.setAge(age);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFixed method, of class Animal.
+     */
+    @Test
+    public void testSetFixed() {
+        System.out.println("setFixed");
+        boolean fix = false;
+        Animal instance = new Animal();
+        instance.setFixed(fix);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLegs method, of class Animal.
+     */
+    @Test
+    public void testSetLegs() {
+        System.out.println("setLegs");
+        int legs = 0;
+        Animal instance = new Animal();
+        instance.setLegs(legs);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setWeight method, of class Animal.
+     */
+    @Test
+    public void testSetWeight() {
+        System.out.println("setWeight");
+        BigDecimal animalWeight = null;
+        Animal instance = new Animal();
+        instance.setWeight(animalWeight);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDateAdded method, of class Animal.
+     */
+    @Test
+    public void testSetDateAdded() {
+        System.out.println("setDateAdded");
+        LocalDate dateAdded = null;
+        Animal instance = new Animal();
+        instance.setDateAdded(dateAdded);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of dateValidator method, of class Animal.
+     */
+    @Test
+    public void testDateValidator() {
+        System.out.println("dateValidator");
+        LocalDate date = null;
+        Animal instance = new Animal();
+        instance.dateValidator(date);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLastFeedingTime method, of class Animal.
+     */
+    @Test
+    public void testSetLastFeedingTime() {
+        System.out.println("setLastFeedingTime");
+        LocalDateTime ldt = null;
+        Animal instance = new Animal();
+        instance.setLastFeedingTime(ldt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of compareTo method, of class Animal.
+     */
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+        Animal other = null;
+        Animal instance = new Animal();
+        int expResult = 0;
+        int result = instance.compareTo(other);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
