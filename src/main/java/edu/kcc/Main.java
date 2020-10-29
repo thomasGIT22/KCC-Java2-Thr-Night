9e09d51147431592524650155014e6c5704435ec
Nathaniel Webber <nathanieljawebber@gmail.com>
2020-10-29 18:09:04 -0500

    Added UIUtility and Main framework

--- src/main/java/edu/kcc/Main.java
+++ src/main/java/edu/kcc/Main.java
@@ -5,9 +5,13 @@
  */
 package edu.kcc;
 
+import edu.kcc.ui.UIUtility;
+import edu.kcc.animal.Animal;
+
 /**
  *
  * @author k0519415
+ * @author Nathaniel Webber
  */
 public class Main {
 
@@ -15,7 +19,54 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UIUtility.showMessage("Program starting...");
+        
+        String menuTitle = "Main Menu";
+        String[] menuOptions = {
+            "1) Add an Animal",
+            "2) Find an Animal",
+            "3) Show All Animals",
+            "4) Update an Animal",
+            "5) Delete an Animal",
+            "6) Exit"
+        };
+        String prompt = "Your choice:";
+        String errorMessage = "Invalid option.  Please try again.";
+        String userChoice;
+        // AnimalDAO dao = AnimalDAO.getAnimalDAO();
+        
+        boolean working = true;
+        while(working) {
+            userChoice = UIUtility.showMenuOptions(menuTitle, prompt, menuOptions);
+            switch(userChoice) {
+                case "1":
+                    // Add an Animal
+                    System.out.println("You added an Animal");
+                    break;
+                case "2":
+                    // Find an Animal
+                    System.out.println("You found the Animal");
+                    break;
+                case "3":
+                    // Show all Animals
+                    System.out.println("You're looking at all the Animals");
+                    break;
+                case "4":
+                    // Update an Animal
+                    System.out.println("You have updated the Animal");
+                    break;
+                case "5":
+                    // Delete an Animal
+                    System.out.println("You have deleted the Animal");
+                    break;
+                case "6":
+                    working = false;
+                    break;
+                default:
+                    UIUtility.showErrorMessage(errorMessage, true);  
+            }
+        }
+        UIUtility.showMessage("\nProgram complete.");
     }
     
 }
